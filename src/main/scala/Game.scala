7c8ba751bd95e381fbed2929caf0930b6ff2ffc3
drewboardman <drew.e.boardman@gmail.com>
2017-11-12 15:42:50 -0500

    it works

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -11,16 +11,15 @@ object Game extends JFXApp {
 
   def start: Unit = {
     var lastTime = 0L
-    var board = Board(BoardTemplates.standard)
+    var board = Board(BoardTemplates.gliderGun)
     val gContext = getGContext(board)
 
     animate(board, gContext)
 
     val timer = AnimationTimer { now: Long =>
-      if (now - lastTime >= 200000000) {
+      if (now - lastTime >= 100000000) {
         lastTime = now
         board = board.tick
-        println(s"\nticking @ time: ${now}\n")
         animate(board, gContext)
       }
     }
@@ -34,12 +33,8 @@ object Game extends JFXApp {
     for (y <- 0 until yBound) {
       val row: Vector[Cell] = board.cellMatrix(y)
       for (x <- 0 until xBound) {
-        val cell = board.cellMatrix(x)(y) // might need to switch these
+        val cell = board.cellMatrix(x)(y)
         val cellGraphic = CellGraphic(x, y, board.width)
-        // println(s"""\n
-        //   cellGraphic.x: ${cellGraphic.x}\n
-        //   cellGraphic.y: ${cellGraphic.y}\n
-        //   cellGraphic.cellSize: ${cellGraphic.cellSize}\n""")
         gContext.setFill(cell.color)
         gContext.fillRect(
           cellGraphic.x,
@@ -52,16 +47,14 @@ object Game extends JFXApp {
   }
 
   private def getGContext(board: Board): GraphicsContext = {
-    val canvas = new Canvas(board.width, board.height)
+    val canvas = new Canvas(Config.boardSideLength, Config.boardSideLength)
     stage = new JFXApp.PrimaryStage {
       scene = new Scene(Config.boardSideLength, Config.boardSideLength) {
         content = canvas
       }
     }
     val gContext = canvas.graphicsContext2D
-
-    gContext.setFill(Color.BLACK)
-    gContext.fillRect(0, 0, Config.boardSideLength, Config.boardSideLength)
+    gContext.fill = Color.BLACK
 
     gContext
   }
